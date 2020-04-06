635405f3a50d6ea7fe3d27b21421c35d1951f4cd
md helal uddin <mdhelaluddin872@gmail.com>
2020-04-06 00:26:32 -0400

    this is first commit

---
+++ src/test/java/CucumberOptions/TestRunner.java
@@ -0,0 +1,24 @@
+package CucumberOptions;
+
+
+
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+// features
+// stepDefination
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features ="src/test/java/features",
+		glue = "stepDefinations")
+public class TestRunner {
+	
+	
+	
+
+}
